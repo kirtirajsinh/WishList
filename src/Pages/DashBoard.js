ac8fc2bed9b85287fe9e5c6b9e025fe4ab677421
kirtirajsinh <kirtiraj3738@gmail.com>
2021-11-21 16:55:49 +0530

    Style DashBoard

--- src/Pages/DashBoard.js
+++ src/Pages/DashBoard.js
@@ -16,27 +16,72 @@ const DashBoard = () => {
         // vvvvvv places the object into the "offers" state 
         setOffers(offersRaw.data.business[0].offers)
     }, [])
+
+    const handleOnclick = (e) => {
+        e.preventDefault()
+        {}
+        console.log("clicked")
+    }
     
     return (
         <div>
-            <Nav />
+                <Nav />
             <h2 className="text-2xl my-2">DashBoard</h2>
-            <div className=" flex flex-row  space-x-5 mx-12 mt-7 h-1/2">
-                <div className="border border-black h-96  w-1/3" >
+            <div className=" flex flex-row  space-x-5 mx-12 mt-7 ">
+                <div className="border border-black w-1/3" >
                     <h3 className="text-xl">WishList</h3>
                     <div className="w-max h-1/2 mx-2 content-between">
                     {offers.map((offer, index) => {
                             return(
-                                <div key={index} className="flex flex-row my-2 border border-black space-x-2">
-                                   <p>{offer.wishlistItem.customer.username}<br />Item:{offer.wishlistItem.item}<br /> Price:{offer.price}<br />latitude: {offer.wishlistItem.lat}, Longitude:{offer.wishlistItem.lng} </p>
+                                <div key={index} className=" m-4 block p-6 bg-white shadow-md border border-gray-200 rounded-lg max-w-sm">
+                                   <p>Name: {offer.wishlistItem.customer.username}<br />Item:{offer.wishlistItem.item}<br /> Price:{offer.price}<br />latitude: {offer.wishlistItem.lat}, Longitude:{offer.wishlistItem.lng} </p>
+                                   <div className="space-x-1">
+                                        <button className="bg-green-500 text-white font-bold py-2 px-4 rounded" onClick={handleOnclick}>Accept </button>
+                                        <button className="bg-red-500 text-white font-bold py-2 px-4 rounded">Decline </button>
+                                 </div>
                                 </div>
                             )
                         })}
+                        
                     </div>
                 </div>
-                <div className="border border-black w-1/3"><h3 className="text-xl">Orders</h3></div>
                 <div className="border border-black w-1/3">
-                    <h3 className="text-xl">Completed Orders</h3>
+                    <h3 className="text-xl">Declined Offers</h3>
+                    <div className="w-max h-1/2 mx-2 content-between">
+                    {offers.map((offer, index) => {
+                        if(!offers.accepted){
+                            return(
+                                <div key={index} className=" m-4 block p-6 bg-white shadow-md border border-gray-200 rounded-lg max-w-sm">
+                                   <p>Name: {offer.wishlistItem.customer.username}<br />Item:{offer.wishlistItem.item}<br /> Price:{offer.price}<br />latitude: {offer.wishlistItem.lat}, Longitude:{offer.wishlistItem.lng} </p>
+                                </div>
+                            )
+                        }
+                        })}
+                        
+                    </div>
+                       
+                        
+                    
+                    </div>
+                <div className="border border-black w-1/3">
+                    <h3 className="text-xl">Accepted Offers</h3>
+                    <div className="w-max h-1/2 mx-2 content-between">
+                    {offers.map((offer, index) => {
+                        if(offers.accepted){
+                            return(
+                                <div key={index} className=" m-4 block p-6 bg-white shadow-md border border-gray-200 rounded-lg max-w-sm">
+                                   <p>Name: {offer.wishlistItem.customer.username}<br />Item:{offer.wishlistItem.item}<br /> Price:{offer.price}<br />latitude: {offer.wishlistItem.lat}, Longitude:{offer.wishlistItem.lng} </p>
+                                   <div className="space-x-1">
+                                        <button className="bg-green-500 text-white font-bold py-2 px-4 rounded" onClick={handleOnclick}>Accept </button>
+                                        <button className="bg-red-500 text-white font-bold py-2 px-4 rounded">Decline </button>
+                                 </div>
+                                </div>
+                            )
+                        }
+                        })}
+                        
+                    </div>
+
                     </div>
             </div>
         </div>
